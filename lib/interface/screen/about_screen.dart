39eabd7414755e359d8be2490bb930de4da48edc
afeeeaa <nurulafifah23032003@gmail.com>
2023-12-01 00:44:15 +0700

    nambah

--- lib/interface/screen/about_screen.dart
+++ lib/interface/screen/about_screen.dart
@@ -7,13 +7,81 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: const Text('Tentang Kami'),
-        ),
-        body: Stack(
-          fit: StackFit.expand,
-          children: [WhiteSpaceWidget()],
-        ));
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Tentang Kami'),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          WhiteSpaceWidget(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 120.0,
+                  height: 120.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/img/logodaur.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  'DaurApp',
+                  style: Theme.of(context).textTheme.headline5!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  'Versi 1.0.0',
+                  style: Theme.of(context).textTheme.caption,
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  'Selamat datang di aplikasi DaurApp. Aplikasi ini bertujuan untuk memudahkan partisipasi masyarakat dalam mendaur ulang sampah.',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                        color: Colors.black,
+                        fontSize: 16.0,
+                      ),
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  'Tim Pengembang:',
+                  style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  'Anisa Meilia',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  'Irfan Priatna',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  'Nurul Afifah',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  'Fachrubi Annafi',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/interface/screen/tnd_screen.dart
+++ lib/interface/screen/tnd_screen.dart
@@ -7,13 +7,67 @@ class TndScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: const Text('Syarat dan Ketentuan'),
-        ),
-        body: Stack(
-          fit: StackFit.expand,
-          children: [WhiteSpaceWidget()],
-        ));
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Syarat dan Ketentuan'),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          WhiteSpaceWidget(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Syarat dan Ketentuan Penggunaan',
+                  style: Theme.of(context).textTheme.headline6!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  'Dengan menggunakan aplikasi ini, Anda setuju untuk mematuhi syarat dan ketentuan berikut:',
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  '1. Penggunaan aplikasi ini harus sesuai dengan hukum yang berlaku.',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  '2. Penggunaan aplikasi ini bertanggung jawab atas informasi yang diberikan.',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  '3. Dilarang keras untuk menggunakan aplikasi ini untuk kegiatan ilegal atau melanggar etika.',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  '4. Setiap pengguna wajib menjaga kerahasiaan informasi akun pribadi.',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  '5. Tidak diperkenankan menggunakan aplikasi ini untuk tujuan komersial tanpa izin tertulis.',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  '6. Aplikasi ini dapat mengumpulkan data pengguna untuk tujuan analisis dan peningkatan layanan.',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  '7. Penggunaan aplikasi ini dianggap penerimaan sepenuhnya terhadap syarat dan ketentuan ini.',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                // ... tambahkan lebih banyak syarat dan ketentuan jika diperlukan
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
